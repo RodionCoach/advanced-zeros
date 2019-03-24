56dab531a8ad6252c7246c1a21660f2a7311032a
Rodion Kochetkov <www.radik@yandex.by>
2019-03-24 23:45:13 +0300

    solution: with recursive factorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let zeroCount = 0;
+  let factNum = factorial(number).toString(base);
+  for( let i = 0; i < factNum.length; i++){
+    if(factNum[i] == 0){
+      zeroCount++;
+    }else{
+      continue;
+    }
+  }
+  return zeroCount;
+}
+
+function factorial(n){
+  return n ? n * factorial(n - 1) : 1;
 }
